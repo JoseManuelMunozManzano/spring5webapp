14ae4910735fda974f4fafaecdc6cb8ee6d12161
Neimerc <Neil_Mercury@yahoo.es>
2020-02-08 08:56:14 +0100

    Added Publisher entity, Spring Data JPA and bootstrapping data

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,9 +1,13 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import guru.springframework.spring5webapp.domain.Address;
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AddressRepository;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +16,14 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+    private final AddressRepository addressRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, AddressRepository addressRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.addressRepository = addressRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -37,8 +45,26 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        Address reillyAddress = new Address("1005 Gravenstein Highway North", "Sebastopol", "CA", "95472");
+        Publisher reilly = new Publisher("O'Reilly", reillyAddress);
+
+        addressRepository.save(reillyAddress);
+        publisherRepository.save(reilly);
+
+        Address pearsonAddress = new Address("1000 Houway South", "Atlanta", "Atlanta", "12345");
+        Publisher pearson = new Publisher("Pearson", pearsonAddress);
+
+        addressRepository.save(pearsonAddress);
+        publisherRepository.save(pearson);
+
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
+
+        System.out.println("Number of Publishers: " + publisherRepository.count());
+
+        System.out.println(addressRepository.findAll());
+        System.out.println(publisherRepository.findAll());
 
     }
 }
