9e3db95c327cf0193c96fd878af99d1a76e516ab
Neimerc <Neil_Mercury@yahoo.es>
2020-02-09 07:29:07 +0100

    Added thymeleaf

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -77,8 +77,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher=" + publisher +
-                ", authors=" + authors +
                 '}';
     }
 
